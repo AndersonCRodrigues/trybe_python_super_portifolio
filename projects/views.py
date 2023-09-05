54cf9d05b88d11d0f19dc03ee74279e75f8baaec
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-09-05 11:47:01 -0300

    req: 02

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializer import ProfileSerializer
+from .models import Profile
 
 # Create your views here.
+
+
+class ProfileViewSet(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
